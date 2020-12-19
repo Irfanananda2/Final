85cc4abbc5346599d98251ab09cfc74ca7643841
Irfanananda2 <58200653+Irfanananda2@users.noreply.github.com>
2020-12-19 20:37:42 +0700

    Add new Model Actor

---
+++ content/models/actor.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=500)
+    bod = models.DateField(null=True, blank=True)
+
+    class Meta:
+        app_label = 'content'
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
