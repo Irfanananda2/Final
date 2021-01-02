e284ab34c7b30bb076bf4b4e36919b72f2a87d82
Irfanananda2 <58200653+Irfanananda2@users.noreply.github.com>
2021-01-02 20:11:43 +0700

    Testing

--- content/urls.py
+++ content/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('country/add', country_controller.add_country, name='add_country'),
     path('country/edit/<int:country_id>', country_controller.edit_country, name='edit_country'),
     path('country/delete/<int:country_id>', country_controller.delete_country, name='delete_country'),
+    path('Testing'),
 ]
